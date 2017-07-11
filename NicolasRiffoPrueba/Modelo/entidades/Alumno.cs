19355b37c91f8df400a3e79225c070bf3bc3e726
NicolasRiffo <nicolas.Riffo@inacapmail.cl>
2017-07-10 23:07:20 -0400

    1.1

---
+++ NicolasRiffoPrueba/Modelo/entidades/Alumno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NicolasRiffoPrueba.Modelo.entidades
+{
+    public class Alumno
+    {
+        public string Rut { get; set; }
+        public string Nombre { get; set; }
+        public string Contraseña { get; set; }
+        public string Email { get; set; }
+        public int Saldo { get; set; }
+        public int Cupo { get; set; }
+        public string Tipo { get; set; }
+    }
+}
---
+++ NicolasRiffoPrueba/Modelo/entidades/Boleta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NicolasRiffoPrueba.Modelo.entidades
+{
+    public class Boleta
+    {
+        public int Id { get; set; }
+        public int Hojas { get; set; }
+        public int Descuento { get; set; }
+        public string Costo { get; set; }
+    }
+}
